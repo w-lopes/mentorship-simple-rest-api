f78fa2a0a4acfe4b54d3f6a489aef5d4bb5a3c10
otarampinelli <otaviorampinellielias@gmail.com>
2022-06-09 18:04:04 -0300

    bagaça está funfando

--- Controller/User.php
+++ Controller/User.php
@@ -4,6 +4,8 @@ namespace Controller;
 
 use Attributes\Middleware;
 use Middlewares\Auth;
+use Database\Connection;
+use Common\Hash;
 class User
 {
     #[Middleware(Auth::class)]
@@ -13,7 +15,24 @@ class User
     }
     public function post()
     {
-        return 'insert data aqui';
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            throw new \InvalidArgumentException('Invalid Email');
+        }
+
+        $options = ["options" => ["regexp" => "/.{6,}/"]];
+
+        if (!filter_var($_POST['password'], FILTER_VALIDATE_REGEXP, $options)) {
+            throw new \InvalidArgumentException('Invalid Password');
+        }
+
+        $connection = new Connection();
+
+        $connection->insert(data: [
+            "email" => $_POST['email'],
+            "password" => Hash::makeHash($_POST['password'])
+        ], table: "users");
+
+        return true;
         // SEND USER NAME AND PASSWORD
     }
 }
--- public/index.php
+++ public/index.php
@@ -13,12 +13,18 @@ $params = array_slice($pieces, 1);
 
 $controllerNamespace = "Controller\\{$class}";
 
-header('Content-Type: application/json');
+// header('Content-Type: application/json');
 
 try {
     Middleware::apply($controllerNamespace, $method);
     $newClass = new $controllerNamespace();
     $result = $newClass->{$method}(...$params);
+} catch (\InvalidArgumentException $res) {
+    $result = [
+        'result' => $res->getMessage()
+    ];
+
+    http_response_code(400);
 } catch (\Throwable $th) {
     $result = [
         'result' => 'Not found'
